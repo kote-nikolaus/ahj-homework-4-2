18c5b395651cdb050ac1557a807d8b6b0eab46ff
kote-nikolaus <>
2020-12-18 23:16:11 +0300

    e2e tests modified

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -56,9 +56,9 @@ describe('Credit Card Validator form', () => {
     });
 
     browser = await puppeteer.launch({
-      headless: false,
-      slowMo: 100,
-      devtools: true,
+    //  headless: false,
+    //  slowMo: 100,
+    //  devtools: true,
     });
     page = await browser.newPage();
   });
